bc31f0198c415cb2369a4656ef1351057e846307
unknown <florent.vigot@edu.devinci.fr>
2022-04-06 19:28:09 +0200

    add just text and style for notification screem

--- lib/presentation/views/screens/main/pages/notification/index.screen.dart
+++ lib/presentation/views/screens/main/pages/notification/index.screen.dart
@@ -1,3 +1,7 @@
+import 'package:app/presentation/styles/colors.dart';
+import 'package:app/presentation/viewmodels/home/room/room.creation.viewmodel.dart';
+import 'package:app/presentation/views/widgets/custom/text_variant.dart';
+import 'package:app/presentation/views/widgets/utilities/icon/pause_defi_icon_icons.dart';
 import 'package:flutter/material.dart';
 
 class NotificationScreen extends StatelessWidget {
@@ -5,6 +9,37 @@ class NotificationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Notification'));
+    return Container(
+        child: Column(
+            children: [
+              Text('Deliver features faster'),
+              Text('Craft beautiful UIs'),
+              Center(
+                child: Container(
+                height: 60,
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                margin: const EdgeInsets.symmetric(horizontal: 40),
+                decoration: const BoxDecoration(
+                color: AppColors.primaryColor,
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: Column(
+                  children: [
+                    Row (
+                      children: [
+                        TextVariant('Prénom', color: Colors.white, fontWeight: FontWeight.bold,
+                        ),
+                        TextVariant('a accepté ton défi'),
+                      ],
+                    ),
+                    TextVariant("Faire des claquettes")
+                  ],
+                )
+                )
+              )
+      ]
+    )
+    );
   }
 }
+
+
